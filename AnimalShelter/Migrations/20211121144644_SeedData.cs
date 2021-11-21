83ffb4847ed8c715d5e8dff71e42c3134607d363
Shane Graff <copellius@gmail.com>
2021-11-21 07:59:10 -0800

    BUGFIX: Fix AnimalsController. Add search by species to AnimalsController

---
+++ AnimalShelter/Migrations/20211121144644_SeedData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Animals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 2, "Female", "Spot", "Dog" },
+                    { 2, 1, "Female", "Molly", "Cat" },
+                    { 3, 2, "Male", "Ribsy", "Dog" },
+                    { 4, 3, "Male", "Spiffy", "Dog" },
+                    { 5, 22, "Male", "Underdog", "Cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Age",
+                table: "Animals",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,6 +9,17 @@ namespace AnimalShelter.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Animal>()
+            .HasData(
+            new Animal { AnimalId = 1, Name = "Spot", Species = "Dog", Age = 2, Gender = "Female" },
+            new Animal { AnimalId = 2, Name = "Molly", Species = "Cat", Age = 1, Gender = "Female" },
+            new Animal { AnimalId = 3, Name = "Ribsy", Species = "Dog", Age = 2, Gender = "Male" },
+            new Animal { AnimalId = 4, Name = "Spiffy", Species = "Dog", Age = 3, Gender = "Male" },
+            new Animal { AnimalId = 5, Name = "Underdog", Species = "Cat", Age = 22, Gender = "Male" }
+      );
+    }   
         public DbSet<Animal> Animals { get; set; }
-    }
+  }
 }
